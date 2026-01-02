7f64c80e51ec66505cb961d5de419822a3d20b46
NinjaCrusader <jeroldpemberton@gmail.com>
2026-01-01 22:14:47 -0500

    Adding all the updated files

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,15 +2,19 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 max_results = 50
 
+@task
 def get_playlist_id():
 
     try:
@@ -37,7 +41,7 @@ def get_playlist_id():
         raise e
     
 
-
+@task
 def get_video_ids(playlist_id):
 
     video_ids = []
@@ -75,7 +79,7 @@ def get_video_ids(playlist_id):
         raise e
     
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -120,7 +124,7 @@ def extract_video_data(video_ids):
         raise e
 
 
-
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/ELT_Project{date.today()}.json"
 
